c37ed6d008c0d109d55e441a09b5abef8abeb1bc
Anna-Anka <anjbekrenewa@gmail.com>
2022-12-01 20:02:56 +0300

    start project

---
+++ src/js/_libs.js
@@ -0,0 +1,11 @@
+// * Swiper
+//import '../../node_modules/swiper/swiper-bundle.min.js'
+
+// * fancybox
+//import '../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
+
+// * rangeslider
+//import '../../node_modules/ion-rangeslider/js/ion.rangeSlider.js'
+
+// * nounslider (price filter vanila js)
+//import '../../node_modules/nouislider/dist/nouislider.min.js'
